b9682e77c601bd35e11d9d7774d23ab0e1d3cb43
William Hamilton <draxy80@gmail.com>
2021-10-29 08:45:06 +1000

    GiftLists done

--- src/Components/GiftLists/GiftLists.js
+++ src/Components/GiftLists/GiftLists.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Container, Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,19 +7,16 @@ import {
   faBirthdayCake,
   faPray,
   faGifts,
-  faCheck, 
-  faTimes
+  faCheck,
+  faTimes,
 } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGem
-} from "@fortawesome/free-regular-svg-icons";
-import {Jumbo} from "../Jumbo/Jumbo";
-
+import { faGem } from "@fortawesome/free-regular-svg-icons";
+import { Jumbo } from "../Jumbo/Jumbo";
+import api from "../../api/user.api";
 import "./GiftLists.css";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 export const GiftLists = () => {
-
   const xmasIcon = <FontAwesomeIcon icon={faCandyCane} />;
   const bdayIcon = <FontAwesomeIcon icon={faBirthdayCake} />;
   const wishIcon = <FontAwesomeIcon icon={faPray} />;
@@ -28,87 +25,156 @@ export const GiftLists = () => {
   const checkIcon = <FontAwesomeIcon icon={faCheck} />;
   const crossIcon = <FontAwesomeIcon icon={faTimes} />;
 
-  const stylered = { color: "red" }
-  const stylegreen = { color: "green" }
+  const stylered = { color: "red" };
+  const stylegreen = { color: "green" };
 
   const history = useHistory();
   const loggedIn = useSelector((state) => state.user.loggedIn);
+  const id = useSelector((state) => state.user.id);
 
-  const handleClickListItem = () => {
-    history.push('/giftlist')
-  }
+  const [allList, setAllList] = useState([]);
+  const [userSubs, setUserSubs] = useState([]);
+  const [userPurchases, setUserPurchases] = useState([]);
+
+  const getAllLists = async () => {
+    const response = await api.get(`/list`);
+    setAllList(response.data);
+  };
+
+  const getUserSubscriptions = async () => {
+    const response = await api.get(`/user/subscriptions/${id}`);
+    if (response.data.length > 0) {
+      setUserSubs(response.data[0].subscriptions);
+    }
+  };
+
+  const getUserPurchases = async () => {
+    const response = await api.get(`/user/purchases/${id}`);
+    console.log(response)
+    if (response.data.length > 0) {
+      setUserPurchases(response.data[0].purchases);
+    }
+  };
+
+  useEffect(() => {
+    getAllLists();
+    if (id){
+      getUserSubscriptions();
+      getUserPurchases();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  const handleClickList = () => {
+    history.push("/giftlist");
+  };
+
+  const displayPurchased = (l_id) => {
+    if (userPurchases?.find(({ list_id }) => list_id === l_id)) {
+      return (
+        <th className="table-giftr-chosen" style={stylegreen}>
+          {checkIcon}
+        </th>
+      );
+    } else {
+      return (
+        <th className="table-giftr-chosen" style={stylered}>
+          {crossIcon}
+        </th>
+      );
+    }
+  };
+
+  const displaySubscribed = (l_id) => {    
+    if (userSubs?.find(({ id }) => id === l_id)) {
+      return (
+        <th className="table-giftr-chosen" style={stylegreen}>
+          {checkIcon}
+        </th>
+      );
+    } else {
+      return (
+        <th className="table-giftr-chosen" style={stylered}>
+          {crossIcon}
+        </th>
+      );
+    }
+  };
+
+  const displayIcon = (type) => {
+    switch (type) {
+      case "xmas":
+        return xmasIcon;
+      case "bday":
+        return bdayIcon;
+      case "wish":
+        return wishIcon;
+      case "wed":
+        return wedIcon;
+      default:
+        return otherIcon;
+    }
+  };
 
   return (
     <>
       <Jumbo />
-    <div className="yourlist-container">
-      <div className="yourlist">
-        <div className="yourlist-title">
-          <h1>GIFTr Lists</h1>
+      <div className="yourlist-container">
+        <div className="yourlist">
+          <div className="yourlist-title">
+            <h1>GIFTr Lists</h1>
+          </div>
+          <div>
+            <p>
+              Here you will find the GIFTr lists that you have subscribed to.
+            </p>
+          </div>
+          <Container className="d-flex flex-column">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th className="table-giftr-icon"></th>
+                  <th className="table-giftr-name">List Name</th>
+                  {loggedIn && (
+                    <th className="table-giftr-created">Subscribed</th>
+                  )}
+                  {loggedIn && (
+                    <th className="table-giftr-created">Purchased Gift</th>
+                  )}
+                  <th className="table-giftr-created">Date Created</th>
+                  <th className="table-giftr-closing">Date Closing</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allList.length > 1 &&
+                  allList.map((list) => {
+                    return (
+                      <tr
+                        key={list.id}
+                        onClick={() => {
+                          handleClickList();
+                        }}
+                      >
+                        <td className="table-giftr-icon">
+                          {displayIcon(list.type)}
+                        </td>
+                        <td className="table-giftr-name">{list.name}</td>
+                        {loggedIn && displaySubscribed(list.id)}
+                        {loggedIn && displayPurchased(list.id)}
+                        <td className="table-giftr-created">
+                          {new Date(list.created_at).toDateString()}
+                        </td>
+                        <td className="table-giftr-closing">
+                          {new Date(list.closing).toDateString()}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Container>
         </div>
-        <div>
-          <p>Here you will find the GIFTr lists that you have subscribed to.</p>
-        </div>
-        <Container className="d-flex flex-column">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th className="table-giftr-icon"></th>
-                <th className="table-giftr-name">List Name</th>
-                { loggedIn && <th className="table-giftr-created">Subscribed</th>}
-                { loggedIn && <th className="table-giftr-created">Purchased Gift</th>}
-                <th className="table-giftr-created">Date Created</th>
-                <th className="table-giftr-closing">Date Closing</th>
-              </tr>
-            </thead>
-            <tbody>
-              
-              <tr onClick={() => {handleClickListItem()}}>
-                <td className="table-giftr-icon">{xmasIcon}</td>
-                <td className="table-giftr-name">Emma's Xmas List</td>
-                {loggedIn && <th className="table-giftr-chosen" style={stylegreen}>{checkIcon}</th>}
-                {loggedIn && <th className="table-giftr-chosen" style={stylegreen}>{checkIcon}</th>}
-                <td className="table-giftr-created">21/10/2021</td>
-                <td className="table-giftr-closing">25/12/2021</td>
-              </tr>
-              
-              <tr>
-                <td className="table-giftr-icon">{bdayIcon}</td>
-                <td className="table-giftr-name">Greg's Birthday List</td>
-                {loggedIn && <th className="table-giftr-chosen red">{crossIcon}</th>}
-                {loggedIn && <th className="table-giftr-chosen red">-</th>}
-                <td className="table-giftr-created">21/10/2021</td>
-                <td className="table-giftr-closing">02/08/2022</td>
-              </tr>
-              <tr>
-                <td className="table-giftr-icon">{wishIcon}</td>
-                <td className="table-giftr-name">Charles' Wish List</td>
-                {loggedIn && <th className="table-giftr-chosen" style={stylegreen}>{checkIcon}</th>}
-                {loggedIn && <th className="table-giftr-chosen red" style={stylered}>{crossIcon}</th>}
-                <td className="table-giftr-created">21/10/2021</td>
-                <td className="table-giftr-closing">21/10/2022</td>
-              </tr>
-              <tr>
-                <td className="table-giftr-icon">{wedIcon}</td>
-                <td className="table-giftr-name">Rhonda and Ketut Wedding List</td>
-                {loggedIn && <th className="table-giftr-chosen red">{crossIcon}</th>}
-                {loggedIn && <th className="table-giftr-chosen red">-</th>}
-                <td className="table-giftr-created">21/10/2021</td>
-                <td className="table-giftr-closing">21/10/2022</td>
-              </tr>
-              <tr>
-                <td className="table-giftr-icon">{otherIcon}</td>
-                <td className="table-giftr-name">John's Gift List</td>
-                {loggedIn && <th className="table-giftr-chosen"  style={stylered}>{crossIcon}</th>}
-                {loggedIn && <th className="table-giftr-chosen"  style={stylered}>-</th>}
-                <td className="table-giftr-created">21/10/2021</td>
-                <td className="table-giftr-closing">21/10/2022</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Container>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
