1099bc5544aafe85e1398a18679a146cd5749c6b
William Hamilton <draxy80@gmail.com>
2021-10-29 07:10:42 +1000

    Fixed spacing

--- src/Components/YourList/YourList.js
+++ src/Components/YourList/YourList.js
@@ -1,19 +1,17 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCandyCane,
   faBirthdayCake,
   faPray,
-  faGifts
+  faGifts,
 } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGem
-} from "@fortawesome/free-regular-svg-icons";
-import {Jumbo} from "../Jumbo/Jumbo";
+import { faGem } from "@fortawesome/free-regular-svg-icons";
+import { Jumbo } from "../Jumbo/Jumbo";
 import api from "../../api/user.api";
 
-import { Link,useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./YourList.css";
@@ -28,80 +26,86 @@ export const YourList = () => {
   const history = useHistory();
   const id = useSelector((state) => state.user.id);
 
-  const [ userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
   const getUserLists = async (id) => {
     const response = await api.get(`/user/lists/${id}`);
-    console.log(response);
     setUserList(response.data[0].lists);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getUserLists(id);
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const handleClickList = (id) => {
     alert(`clicked list: ${id}`);
-  }
+    history.push("/yourlists");
+  };
 
   const displayIcon = (type) => {
     switch (type) {
-      case 'xmas':
+      case "xmas":
         return xmasIcon;
-      case 'bday':
+      case "bday":
         return bdayIcon;
-      case 'wish':
-        return wishIcon
-      case 'wed':
+      case "wish":
+        return wishIcon;
+      case "wed":
         return wedIcon;
       default:
         return otherIcon;
     }
-    
-  }
+  };
 
   return (
     <>
-    <Jumbo />
-    <div className="yourlist-container">
-      <div className="yourlist">
-        <div className="yourlist-title">
-          <h1>Your GIFTr Lists</h1>
-        </div>
-        <Container className="d-flex flex-column">
-          <Link to="/createnewlist">
-            <Button className="btnCoffee mt-2">Create a New List</Button>
-          </Link>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th className="table-icon"></th>
-                <th className="table-name">List Name</th>
-                <th className="table-date-created">Date Created</th>
-                <th className="table-date-closing">Date Closing</th>
-              </tr>
-            </thead>
-            <tbody>
-              
-              {userList.length > 0 && userList.map((list) => {
-              return (
-                <tr key={list.id} onClick={() => {handleClickList(list.id)}}>
-                  <td className="table-icon">{displayIcon(list.type)}</td>
-                  <td className="table-name">{list.name}</td>
-                  <td className="table-date-created">{new Date(list.created_at).toDateString()}</td>
-                  <td className="table-date-closing">{new Date(list.closing).toDateString()}</td>
+      <Jumbo />
+      <div className="yourlist-container">
+        <div className="yourlist">
+          <div className="yourlist-title">
+            <h1>Your GIFTr Lists</h1>
+          </div>
+          <Container className="d-flex flex-column">
+            <Link to="/createnewlist">
+              <Button className="btnCoffee mt-2">Create a New List</Button>
+            </Link>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th className="table-icon"></th>
+                  <th className="table-name">List Name</th>
+                  <th className="table-date-created">Date Created</th>
+                  <th className="table-date-closing">Date Closing</th>
                 </tr>
-              );
-
-              })}
-              
-            </tbody>
-          </Table>
-        </Container>
+              </thead>
+              <tbody>
+                {userList.length > 0 &&
+                  userList.map((list) => {
+                    return (
+                      <tr
+                        key={list.id}
+                        onClick={() => {
+                          handleClickList(list.id);
+                        }}
+                      >
+                        <td className="table-icon">{displayIcon(list.type)}</td>
+                        <td className="table-name">{list.name}</td>
+                        <td className="table-date-created">
+                          {new Date(list.created_at).toDateString()}
+                        </td>
+                        <td className="table-date-closing">
+                          {new Date(list.closing).toDateString()}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Container>
+        </div>
       </div>
-    </div>
     </>
   );
 };
